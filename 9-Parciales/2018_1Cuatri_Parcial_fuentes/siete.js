fc89be4bc6dbe866afd3ad2477ca7cd04007d828
belendaguerre <40828610+belendaguerre@users.noreply.github.com>
2018-08-05 23:40:12 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+	var nota, sexo, suma=0, promedio, contador=0;
+
+	for(var i=0; i<5; i++)
+	{
+		do
+		{
+			nota=parseInt(prompt("Ingrese la nota"));
+		}
+		while (nota < 0 || nota >10);
+		do
+		{
+			sexo = parseInt(prompt("Ingrese sexo (f , m)"));
+		}
+		while(toUpperCase(sexo) !="F" && toUpperCase (sexo) != "M");
+		suma = suma + nota;
+		if(i==0)
+		{
+			menor=nota;
+		}
+		if(toUpperCase (sexo) == "M" && nota >= 6)
+		{
+			contador = contador + 1;
+		}
+
+	} 
+	promedio = suma / 5;
+	alert (promedio);
+	alert (menor);
+	alert (contador);
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var largo, ancho, resultado;
+
+		largo = parseInt(prompt("Ingrese el largo"));
+		ancho = parseInt(prompt("Ingrese el ancho"));
+
+		resultado = (largo + ancho) * 2;
+
+		alert(resultado);
 
 }
+
